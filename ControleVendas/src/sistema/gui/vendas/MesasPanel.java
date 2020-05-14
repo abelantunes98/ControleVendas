33f3d49b8b75eb5e55d1911656e4520b97461107
abelantunes98 <abel.neto@ccc.ufcg.edu.br>
2020-05-14 01:52:24 -0300

    Adicionando anotaçoes

--- ControleVendas/src/sistema/gui/vendas/MesasPanel.java
+++ ControleVendas/src/sistema/gui/vendas/MesasPanel.java
@@ -28,16 +28,12 @@ public class MesasPanel extends JPanel {
 	private static final long serialVersionUID = -2996858019062843508L;
 	
 	private JTextField valCodigoMesa;
-	private JTextField valCodigosMesas;
 	private final ButtonGroup buttonGroupOpcao = new ButtonGroup();
 	private JTable tableMesas;
 	
 	private JLabel codigoMesa;
-	private JLabel codigosMesas;
-	private JLabel codigosMesas2;
 	private JRadioButton rdbtnOcuparMesa;
 	private JRadioButton rdbtnLiberarMesa ;
-	private JRadioButton rdbtnJuntarMesas;
 	private JButton btnConfirmar;
 	private JButton btnLimpar;
 	
@@ -49,6 +45,7 @@ public class MesasPanel extends JPanel {
 	
 	private Principal principal;
 	private JanelaFrame frame;
+	private JLabel opcaoDesejada;
 
 	/**
 	 * Create the panel.
@@ -76,24 +73,10 @@ public class MesasPanel extends JPanel {
 		add(valCodigoMesa);
 		valCodigoMesa.setColumns(10);
 		
-		codigosMesas = new JLabel("C\u00F3digos das mesas:");
-		codigosMesas.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		codigosMesas.setBounds(176, 474, 222, 68);
-		codigosMesas.setEnabled(false);
-		add(codigosMesas);
-		
-		codigosMesas2 = new JLabel("(Separados por espa\u00E7o)");
-		codigosMesas2.setFont(new Font("Tahoma", Font.PLAIN, 19));
-		codigosMesas2.setBounds(186, 523, 212, 41);
-		codigosMesas2.setEnabled(false);
-		add(codigosMesas2);
-		
-		valCodigosMesas = new JTextField();
-		valCodigosMesas.setFont(new Font("Tahoma", Font.PLAIN, 23));
-		valCodigosMesas.setBounds(413, 493, 288, 41);
-		add(valCodigosMesas);
-		valCodigosMesas.setColumns(10);
-		valCodigosMesas.setEnabled(false);
+		opcaoDesejada = new JLabel("Op\u00E7\u00E3o desejada:");
+		opcaoDesejada.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		opcaoDesejada.setBounds(176, 558, 210, 41);
+		add(opcaoDesejada);
 		
 		this.valorTotal = new JLabel("Valor total:");
 		valorTotal.setFont(new Font("Tahoma", Font.PLAIN, 28));
@@ -110,29 +93,23 @@ public class MesasPanel extends JPanel {
 		rdbtnOcuparMesa = new JRadioButton("Ocupar mesa");
 		buttonGroupOpcao.add(rdbtnOcuparMesa);
 		rdbtnOcuparMesa.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		rdbtnOcuparMesa.setBounds(243, 686, 155, 41);
+		rdbtnOcuparMesa.setBounds(293, 632, 155, 41);
 		add(rdbtnOcuparMesa);
 		
 		rdbtnLiberarMesa = new JRadioButton("Liberar mesa");
 		buttonGroupOpcao.add(rdbtnLiberarMesa);
 		rdbtnLiberarMesa.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		rdbtnLiberarMesa.setBounds(483, 686, 174, 41);
+		rdbtnLiberarMesa.setBounds(525, 632, 174, 41);
 		add(rdbtnLiberarMesa);
-		
-		rdbtnJuntarMesas = new JRadioButton("Juntar mesas");
-		buttonGroupOpcao.add(rdbtnJuntarMesas);
-		rdbtnJuntarMesas.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		rdbtnJuntarMesas.setBounds(717, 686, 169, 41);
-		add(rdbtnJuntarMesas);
 		
 		btnConfirmar = new JButton("Confirmar");
 		btnConfirmar.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		btnConfirmar.setBounds(283, 846, 169, 55);
+		btnConfirmar.setBounds(277, 774, 169, 55);
 		add(btnConfirmar);
 		
 		btnLimpar = new JButton("Limpar");
 		btnLimpar.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		btnLimpar.setBounds(569, 846, 169, 55);
+		btnLimpar.setBounds(564, 774, 169, 55);
 		add(btnLimpar);
 		
 		this.btnPago = new JButton("Pago");
@@ -156,9 +133,6 @@ public class MesasPanel extends JPanel {
 			public void actionPerformed(ActionEvent arg0) {
 				codigoMesa.setEnabled(true);
 				valCodigoMesa.setEnabled(true);
-				valCodigosMesas.setEnabled(false);
-				codigosMesas.setEnabled(false);
-				codigosMesas2.setEnabled(false);
 			}
 		});	
 		
@@ -167,23 +141,10 @@ public class MesasPanel extends JPanel {
 			public void actionPerformed(ActionEvent arg0) {
 				codigoMesa.setEnabled(true);
 				valCodigoMesa.setEnabled(true);
-				valCodigosMesas.setEnabled(false);
-				codigosMesas.setEnabled(false);
-				codigosMesas2.setEnabled(false);
 			}
 		});
 		
-		rdbtnJuntarMesas.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				codigoMesa.setEnabled(false);
-				valCodigoMesa.setEnabled(false);
-				valCodigosMesas.setEnabled(true);
-				codigosMesas.setEnabled(true);
-				codigosMesas2.setEnabled(true);
-			}
-		});	
-		
+
 		btnLimpar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -244,12 +205,10 @@ public class MesasPanel extends JPanel {
 	private void carregaInformacoesPagamento(String codigoMesa) {
 		try {
 			this.codigoMesa.setEnabled(false);
-			this.codigosMesas.setEnabled(false);
+			this.opcaoDesejada.setEnabled(false);
 			this.valCodigoMesa.setEnabled(false);
-			this.valCodigosMesas.setEnabled(false);
 			this.rdbtnOcuparMesa.setEnabled(false);
 			this.rdbtnLiberarMesa.setEnabled(false);
-			this.rdbtnJuntarMesas.setEnabled(false);
 			this.btnConfirmar.setEnabled(false);
 			this.btnLimpar.setEnabled(false);
 			
@@ -261,9 +220,15 @@ public class MesasPanel extends JPanel {
 			this.btnPago.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
-					liberaMesa(codigoMesa);
-					reload();
-					JOptionPane.showMessageDialog(null, "Mesa liberada.", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+					try {
+						liberaMesa(codigoMesa);
+						reload();
+						JOptionPane.showMessageDialog(null, "Mesa liberada.", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+					}
+					catch (Exception e) {
+						JOptionPane.showMessageDialog(null, e.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
+					}
+					
 				}
 			});
 			
@@ -294,20 +259,19 @@ public class MesasPanel extends JPanel {
 	private void limparCampos() {
 		
 		valCodigoMesa.setText("");
-		valCodigosMesas.setText("");
 	}
 	
 	
 	private void confirmar() {
 		
 		try {
-			//String codigosMesas = valCodigosMesas.getText();
 			
+			// Verificando se algum rádio button foi selecionado.
 			if (rdbtnOcuparMesa.isSelected() || rdbtnLiberarMesa.isSelected()) {
 				
 				String codigoMesaStr = valCodigoMesa.getText();		
 				if ( codigoMesaStr.equals("") ) {
-					throw new IllegalArgumentException("Campos passados vazios!");
+					throw new IllegalArgumentException("Campo passado vazio!");
 				}
 				
 				if (rdbtnOcuparMesa.isSelected()) {
@@ -316,9 +280,19 @@ public class MesasPanel extends JPanel {
 				}
 				else {
 					
+					/*
+					 * Verificações de erro especiais.
+					 * São necessárias pois há iterações antes da chamada do
+					 * backend.
+					 */
 					if (!this.principal.retornaMesaExiste(codigoMesaStr)) {
 						throw new IllegalArgumentException("Mesa inexistente.");
-					}				
+					}
+					
+					if (this.principal.retornaMesaDisponivel(codigoMesaStr)) {
+						throw new IllegalArgumentException("A mesa não está ocupada.");
+					}
+					
 					int resposta = JOptionPane.showConfirmDialog(null, "Deseja calcular os gastos da mesa?", 
 							"Faturamento", JOptionPane.YES_OPTION);
 					if (resposta == JOptionPane.NO_OPTION) {
@@ -336,6 +310,9 @@ public class MesasPanel extends JPanel {
 						this.carregaInformacoesPagamento(codigoMesaStr);
 					}
 				}
+			}
+			else {
+				throw new IllegalArgumentException("Selecione uma operação.");
 			}
 		}
 		catch (Exception e) {
